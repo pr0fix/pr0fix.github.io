6a36a75edda35505433774d9fd6bba624a3f1f5d
pr0fix <bgx152@myy.haaga-helia.fi>
2024-11-19 14:52:44 +0200

    Change image paths to work with gh-pages

--- src/components/Projects/CoffeeCompanion.tsx
+++ src/components/Projects/CoffeeCompanion.tsx
@@ -18,11 +18,11 @@ const projectData: ProjectItem[] = [
     description:
       "React Native mobile app where users can find, review and add coffee shops to favorites.",
     screenshots: [
-      "../screenshots/CoffeeCompanion/homepage.png",
-      "../screenshots/CoffeeCompanion/bottomsheet.png",
-      "../screenshots/CoffeeCompanion/profile_page.png",
-      "../screenshots/CoffeeCompanion/edit_profile.png",
-      "../screenshots/CoffeeCompanion/sign_up.png",
+      "images/homepage.png",
+      "images/bottomsheet.png",
+      "images/profile_page.png",
+      "images/edit_profile.png",
+      "images/sign_up.png",
     ],
     link: "https://github.com/pr0fix/CoffeeCompanion",
   },
