d678fb86803b86de4544c6aee52c03d25f605550
pr0fix <bgx152@myy.haaga-helia.fi>
2025-09-21 09:33:19 +0300

    add favicon & update text styling

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,17 +1,28 @@
 const Introduction: React.FC = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-center items-center gap-10 mt-10 px-6 max-w-6xl mx-auto">
-      <div className="flex-1 max-w-3xl">
-        <h1 className="mb-4">Hello! I am Jani</h1>
-        <p className="text-base md:text-lg leading-relaxed">
-          A Junior Marketing Technology Developer with background in web
-          development and a Bachelor's in Information Technology. I love
-          learning and working with new technologies and my superpower as a
-          developer is my can-do attitude.
+    <div className="flex flex-col lg:flex-row justify-center items-center gap-10 mt-16 px-6 max-w-6xl mx-auto">
+      <div className="flex-1 max-w-[650px]">
+        <h1 className="text-hero mb-6 text-gray-800">
+          Hello! <br />
+          <span className="text-blue-600">I am Jani!</span>
+        </h1>
+        <p className="text-lg md:text-xl leading-relaxed text-gray-700 font-medium">
+          A {" "}
+          <span className="font-semibold text-gray-800">
+            Junior Marketing Technology Developer
+          </span>{" "}
+          with background in web development and a Bachelor's in Information
+          Technology. I love learning and working with new technologies and my
+          superpower as a developer is my{" "}
+          <span className="font-semibold text-blue-600">can-do attitude</span>.
         </p>
       </div>
       <div className="shrink-0">
-        <img src="/cartoon_portrait.png" alt={"self portrait"} className="w-auto h-96 rounded-lg shadow-lg object-cover" />
+        <img
+          src="/cartoon_portrait.png"
+          alt={"self portrait"}
+          className="w-auto h-96 rounded-lg shadow-lg object-cover"
+        />
       </div>
     </div>
   );
--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -4,8 +4,8 @@ const SectionDivider: React.FC<SectionDividerProps> = ({
   title,
 }: SectionDividerProps) => {
   return (
-    <div className="offset-border flex justify-between py-6 mt-10 bg-[var(--secondary-bg-color)]">
-      <h2 className="ml-6">{title}</h2>
+    <div className="offset-border flex justify-between py-6 my-16 bg-[var(--secondary-bg-color)]">
+      <h2 className="text-section-title ml-6">{title}</h2>
     </div>
   );
 };
